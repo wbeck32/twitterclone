f510051fa86ba42eba7524fb90651310bb6deb12
Wendy Beck <webeck@gmail.com>
2015-02-05 11:40:10 -0800

    Let ginger take the wheel

--- routes/index.js
+++ routes/index.js
@@ -1,9 +1,31 @@
+var Promise = require('bluebird');
+
 var express = require('express');
 var router = express.Router();
+var cookie = require('cookie-parser');
+
+var env = process.env.NODE_ENV || 'development';
+var knexConfig = require('../knexfile.js')[env];
+var knex = require('knex')(knexConfig);
+var bookshelf = require('bookshelf')(knex);
+
 
 /* GET home page. */
 router.get('/', function(req, res, next) {
+    //check cookie and act accordingly
   res.render('index', { title: 'Express' });
+});
+
+router.post('/',function(req,res,next){
+    res.cookie('user_name',req.body.user_name ); 
+    res.cookie('password',req.body.password);
+
+    var Users = bookshelf.Model.extend ({
+        tableName: 'users'
+    });
+    Users.forge({ user_name: req.body.user_name, password:req.body.password }).save().then(function(user) {
+        console.log('user added %j',user);
+    });
 });
 
 module.exports = router;
